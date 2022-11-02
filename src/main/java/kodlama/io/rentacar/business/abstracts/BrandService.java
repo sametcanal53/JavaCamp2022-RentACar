40a71bfeb0cb1072a2482a5ce393a0f9895f9bc1
Sametcan <sametcanal53@hotmail.com>
2022-11-03 00:27:59 +0300

    Service

---
+++ src/main/java/kodlama/io/rentacar/business/abstracts/BrandService.java
@@ -0,0 +1,11 @@
+package kodlama.io.rentacar.business.abstracts;
+
+import kodlama.io.rentacar.business.requests.CreateBrandRequest;
+import kodlama.io.rentacar.business.responses.GetAllBrandsResponse;
+
+import java.util.List;
+
+public interface BrandService {
+    List<GetAllBrandsResponse> getAll();
+    void add(CreateBrandRequest createBrandRequest);
+}
